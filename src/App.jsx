894c029465316c9f6abcf0ea3819be58474f4d78
tuzgbach <tungbach.tts@gmail.com>
2025-02-07 17:42:10 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Newtask from "./component/newtask";
+import Newtask from "./component/Newtask";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Newtask />} />
+          <Route path="/new" element={<Newtask />} />
         </Routes>
       </BrowserRouter>
     </div>
